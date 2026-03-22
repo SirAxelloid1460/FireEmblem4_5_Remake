ce66cfe5e1fd86ad7fe0c2ecc4065787b003c991
Fire & Soul Studios <82539231+SirAxelloid1460@users.noreply.github.com>
2026-03-22 11:00:39 +0100

    FE5 Branch Upload
    FE5 v0.1178

---
+++ app/utilities/typing.py
@@ -0,0 +1,22 @@
+from typing import Tuple
+
+try:
+    from typing_extensions import Protocol
+except (ModuleNotFoundError, ImportError):
+    Protocol = object
+    print("You are missing the `typing-extensions` module.\nTry running `pip install -r requirements_editor.txt`")
+
+try:
+    from typing_extensions import override
+except (ModuleNotFoundError, ImportError):
+    def override(func):
+        return func
+    print("You are missing the `typing-extensions` module.\nTry running `pip install -r requirements_editor.txt`")
+
+Point = Tuple[int, int]
+Segment = Tuple[Point, Point]
+
+NID = str
+UID = int
+Color3 = Tuple[int, int, int]
+Color4 = Tuple[int, int, int, int]
